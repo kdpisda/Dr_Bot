b0cadb9c611b445845fee7a784f118c88a89fb1a
Pravesh <pgill.jjn@gmail.com>
2018-01-28 06:48:21 +0530

    final file after solving all the bugs

--- utils.py
+++ utils.py
@@ -5,16 +5,22 @@ from pymongo import MongoClient
 
 
 #all images we are gonna use
-IMG0 = ""
-IMG1 = ""
-IMG2 = ""
-IMG3 = ""
-IMG4 = ""
-IMG5 = ""
-IMG6 = ""
-IMG7 = ""
-IMG8 = ""
-IMG9 = ""
+IMG10 = "https://thumb1.shutterstock.com/display_pic_with_logo/384598/609220520/stock-vector-young-caucasian-doctor-listening-to-chest-of-patient-with-stethoscope-adult-patient-visiting-609220520.jpg"
+IMG1 = "https://thumb9.shutterstock.com/display_pic_with_logo/764932/524745832/stock-vector-medicine-concept-with-doctor-and-patient-in-flat-style-isolated-on-white-background-doctor-524745832.jpg"
+IMG2 = "https://healthcarecommunication.files.wordpress.com/2010/10/patients-wecareaboutyou.jpg"
+IMG3 = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTHZwqpPs5x21LPveLU_iipMEBW0P6z38Y33tcmPT-Wxbtq0ox6Jw"
+IMG4 = "https://comps.canstockphoto.com/doctor-check-up-patient-eps-vectors_csp15063934.jpg"
+IMG5 = "https://payload260.cargocollective.com/1/15/490814/7505103/Jenny_doctor2_650.png"
+IMG6 = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTS797KiH42JcdFeYeM3iW5ZUazhlAp3DGhV26lKGoBlJa7CQBY"
+IMG7 = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRh_bdRy-kEulLXLFXBuRckpLPbzoDsLR5kmJlYuWzTXMQBJgqYTA"
+IMG8 = "https://thumbs.dreamstime.com/b/cartoon-doctor-patient-isolated-beautiful-illustration-children-50339560.jpg"
+IMG9 = "https://st2.depositphotos.com/1037178/6546/v/950/depositphotos_65469127-stock-illustration-doctor-check-up-patient-medical.jpg"
+
+#images of pills
+IMGP1 = "https://3.imimg.com/data3/VG/IA/MY-123441/pharmaceutical-tablet-250x250.jpg"
+IMGP2 = "https://cdn.pixabay.com/photo/2016/12/06/04/22/pills-1885550_960_720.jpg"
+IMGP3 = "http://i.dailymail.co.uk/i/pix/2016/04/18/15/334C4E4F00000578-3545994-Over_the_counter_treatments_for_cold_flu_heartburn_and_sleeping_-a-43_1460990970115.jpg"
+IMGP4 = "http://5.imimg.com/data5/EC/VN/MY-5449970/vitamin-d3-tablets-250x250.jpg"
 
 
 
@@ -29,10 +35,10 @@ db = client.get_database("bot")
 symp=db.symptom_store
 
 #medico api endpoint
-hack36medico_endpoint = "private"
+hack36medico_endpoint = "https://cdfc3125.ngrok.io/isabel"
 
 #medicine api endpoint
-hack36medicine_endpoint = "private"
+hack36medicine_endpoint = "http://2fa2c9ed.ngrok.io/1mg"
 
 # Available age categories
 age_categories = [('newborn', '1'),('infant', '2'),('younger child','3'),('older child','10'),('adolescent','4'),('young adult','7'),('adult below 40','5'),('adult below 50','8'),('adult below 65','9'),('senior','6')]
@@ -183,7 +189,30 @@ def fetch_reply(query, session_id):
 					element['subtitle'] = "Not serious"
 				elif article['flag'] == 1:
 					element['subtitle'] = "serious diseases"
-				element['image_url'] = "https://drive.google.com/file/d/1VF0vdGeDTbGuaCB-2uKX3mbzqvSPZXy_/view?usp=sharing"#article['img']
+				if article['flag'] == 0:
+					element['subtitle'] = "Not serious"
+				elif article['flag'] == 1:
+					element['subtitle'] = "serious diseases"
+				if count == 1:
+					element['image_url'] = IMG1
+				elif count == 2:
+					element['image_url'] = IMG2
+				elif count == 3:
+					element['image_url'] = IMG3
+				elif count == 4:
+					element['image_url'] = IMG4
+				elif count == 5:
+					element['image_url'] = IMG5
+				elif count == 6:
+					element['image_url'] = IMG6
+				elif count == 7:
+					element['image_url'] = IMG7
+				elif count == 8:
+					element['image_url'] = IMG8
+				elif count == 9:
+					element['image_url'] = IMG9
+				else:
+					element['image_url'] = IMG10
 				#element['default_action'] = {"webview_height_ratio":"tall"}
 				element['buttons'] = [{
 					"type":"web_url",
@@ -220,17 +249,25 @@ def fetch_reply(query, session_id):
 			print("came from 1mg")
 			final_list = []
 
-			##count = 0
+			count = 0
 			for article in final_diseases:
-				#if count > 9:
-					#break
-				#count += 1
+				if count > 4:
+					break
+				count += 1
 				element = {}
 				element['title'] = article['name']
 				element['item_url'] = article['link']
 				element['subtitle'] = "price : Rs" + article['price'] +'\n' + 'No. of Tablets : ' + article['tablets']
-				element['image_url'] = "https://drive.google.com/file/d/1VF0vdGeDTbGuaCB-2uKX3mbzqvSPZXy_/view?usp=sharing"#article['img']
+				#element['image_url'] = "https://drive.google.com/file/d/1VF0vdGeDTbGuaCB-2uKX3mbzqvSPZXy_/view?usp=sharing"#article['img']
 				#element['default_action'] = {"webview_height_ratio":"tall"}
+				if count ==1:
+					element['image_url'] = IMGP1
+				elif count ==2:
+					element['image_url'] = IMGP2
+				elif count ==3:
+					element['image_url'] = IMGP3
+				elif count ==4:
+					element['image_url'] = IMGP4
 				element['buttons'] = [{
 					"type":"web_url",
 					"title":"Buy Now",
